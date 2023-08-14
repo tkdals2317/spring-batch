98b3056b434cf7eab637d88cc5d715a1edb07ecc
lsm0506 <tkdals2317@gmail.com>
2023-08-14 21:30:07 +0900

    Initial Commit

--- src/test/java/io/spring/springbatch/SpringBatchApplicationTests.java
+++ src/test/java/io/spring/springbatch/SpringBatchApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
