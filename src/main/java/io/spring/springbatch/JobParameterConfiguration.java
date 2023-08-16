d8903cb6dbb59b214d1128d52ca6a40754151d04
lsm0506 <lsm0506@midasin.com>
2023-08-16 14:58:58 +0900

    Part3.1.3 소스코드 업로드

--- src/main/java/io/spring/springbatch/JobParameterConfiguration.java
+++ src/main/java/io/spring/springbatch/JobParameterConfiguration.java
@@ -3,6 +3,7 @@ package io.spring.springbatch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -13,9 +14,12 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Date;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Configuration
-public class JobInstanceConfiguration {
+public class JobParameterConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -34,16 +38,31 @@ public class JobInstanceConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        JobInstance jobInstance = contribution.getStepExecution().getJobExecution().getJobInstance();
-                        System.out.println("jobInstance.getId() : " + jobInstance.getId());
-                        System.out.println("jobInstance.getInstanceId() : " + jobInstance.getInstanceId());
-                        System.out.println("jobInstance.getJobName() : " + jobInstance.getJobName());
-                        System.out.println("jobInstance.getJobVersion : " + jobInstance.getVersion());
+                        // contribution에서 뽑아내기(JobParameter로 반환)
+                        JobParameters jobParameters = contribution.getStepExecution().getJobParameters();
+                        String name = jobParameters.getString("name");
+                        long seq = jobParameters.getLong("seq");
+                        Date date = jobParameters.getDate("date");
+                        Double age = jobParameters.getDouble("age");
+                        System.out.println("===========================");
+                        System.out.println("name:" + name);
+                        System.out.println("seq: " + seq);
+                        System.out.println("date: " + date);
+                        System.out.println("age: " + age);
+                        System.out.println("===========================");
+
+                        // chunkContext에서 뽑아내기(Map으로 반환) : 값만 확인할 때 사용
+                        Map<String, Object> jobParameters2 = chunkContext.getStepContext().getJobParameters();
+                        String name2 = (String)jobParameters2.get("name");
+                        long seq2 = (long)jobParameters2.get("seq");
+
+                        System.out.println("step1 has executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
+
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
--- src/main/java/io/spring/springbatch/JobParameterTest.java
+++ src/main/java/io/spring/springbatch/JobParameterTest.java
@@ -5,14 +5,15 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 @RequiredArgsConstructor
-public class JobRunner implements ApplicationRunner {
+public class JobParameterTest implements ApplicationRunner {
 
     private final JobLauncher jobLauncher;
 
@@ -20,12 +21,13 @@ public class JobRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("name", "user1")
-//                .addDate("reqDate", new Date())
+                .addLong("seq", 2L)
+                .addDate("date", new Date())
+                .addDouble("age", 16.5)
                 .toJobParameters();
 
-        jobLauncher.run(job,jobParameters);
+        jobLauncher.run(job, jobParameters);
     }
 }
