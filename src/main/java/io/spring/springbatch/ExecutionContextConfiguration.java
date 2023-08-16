94b00b1c946c5e2552f1ccc19b1b1c62693ef5b8
lsm0506 <tkdals2317@gmail.com>
2023-08-16 22:13:02 +0900

    Part3.3 소스 코드 업로드

--- src/main/java/io/spring/springbatch/ExecutionContextConfiguration.java
+++ src/main/java/io/spring/springbatch/ExecutionContextConfiguration.java
@@ -5,24 +5,30 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class StepExecutionConfiguration {
+public class ExecutionContextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
 
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
+
+
     @Bean
-    public Job job(){
+    public Job Batchjob(){
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
                 .next(step3())
+                .next(step4())
                 .build();
     }
 
@@ -31,21 +37,14 @@ public class StepExecutionConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step1 has executed!");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(executionContextTasklet1)
                 .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step2 has executed!");
-                    //throw new RuntimeException("step2 has failed");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(executionContextTasklet2)
                 .build();
     }
 
@@ -53,10 +52,14 @@ public class StepExecutionConfiguration {
     @Bean
     public Step step3() {
         return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">> step3 has executed!");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(executionContextTasklet3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(executionContextTasklet4)
                 .build();
     }
 }
