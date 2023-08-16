990331607abf8f1ca91ca3aae2381215a44a9dd1
lsm0506 <lsm0506@midasin.com>
2023-08-16 12:04:13 +0900

    Part3.1.2 소스코드 업로드

--- src/main/java/io/spring/springbatch/JobRunner.java
+++ src/main/java/io/spring/springbatch/JobRunner.java
@@ -5,7 +5,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -23,9 +22,8 @@ public class JobRunner implements ApplicationRunner {
 
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("name", "user1")
-//                .addDate("reqDate", new Date())
                 .toJobParameters();
 
-        jobLauncher.run(job,jobParameters);
+        jobLauncher.run(job, jobParameters);
     }
 }
