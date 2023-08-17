000ddf99694bfdf2e8d5146bc09ec698f16852d3
lsm0506 <tkdals2317@gmail.com>
2023-08-17 23:24:59 +0900

    Part4.3.1.3.3 섹션 6 사용자 정의 ExitStatus

--- src/main/java/io/spring/springbatch/CustomExitStatusConfiguration.java
+++ src/main/java/io/spring/springbatch/CustomExitStatusConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class TransitionConfiguration {
+public class CustomExitStatusConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -26,15 +26,8 @@ public class TransitionConfiguration {
                 .start(step1())
                     .on("FAILED")
                     .to(step2())
-                    .on("FAILED")
+                    .on("PASS") // PASS가 아닐 경우 어떻게 하라고 정의되있지 않아 실패로 간주하여 JOB_EXECUTION 테이블에 FAILED로 저장
                     .stop()
-                .from(step1())
-                    .on("*")
-                    .to(step3())
-                    .next(step4())
-                .from(step2())
-                    .on("*")
-                    .to(step5())
                 .end()
                 .build();
     }
@@ -47,7 +40,8 @@ public class TransitionConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println(">> step1 has executed");
-                        contribution.setExitStatus(ExitStatus.FAILED);
+                        //contribution.setExitStatus(ExitStatus.FAILED);
+                        contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -60,46 +54,10 @@ public class TransitionConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println(">> step2 has executed");
-                        contribution.setExitStatus(ExitStatus.FAILED);
                         return RepeatStatus.FINISHED;
                     }
                 })
-                .build();
-    }
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step3 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step4 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step5 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .listener(new PassingCheckingListener())
                 .build();
     }
 
