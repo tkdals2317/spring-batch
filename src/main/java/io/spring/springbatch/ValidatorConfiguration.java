ba0d869a224d715670b511662d0bc1c8491e52bc
lsm0506 <lsm0506@midasin.com>
2023-08-17 14:30:30 +0900

    Part4.2.2.3 SimpleJob - validator()

--- src/main/java/io/spring/springbatch/ValidatorConfiguration.java
+++ src/main/java/io/spring/springbatch/ValidatorConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class StartNextConfiguration {
+public class ValidatorConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -23,6 +24,8 @@ public class StartNextConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
+//                .validator(new CustomJobParametersValidator())
+                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
                 .build();
     }
 
