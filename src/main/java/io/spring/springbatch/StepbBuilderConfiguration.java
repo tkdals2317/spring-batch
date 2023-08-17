4fe398d218a44b02ab14d71fa6531579a13c570e
lsm0506 <lsm0506@midasin.com>
2023-08-17 15:48:49 +0900

    Part4.2.3 SimpleBuilderFactory

---
+++ src/main/java/io/spring/springbatch/StepbBuilderConfiguration.java
@@ -0,0 +1,119 @@
+package io.spring.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class StepbBuilderConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob(){
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        // TaskletStepBuilder 구현체 생성
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step1 has executed!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        // SimpleStepBuilder 구현체 생성
+        return stepBuilderFactory.get("step2")
+                .<String, String> chunk(3)
+                .reader(new ItemReader<String>() {
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        return null;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String s) throws Exception {
+                        return null;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> list) throws Exception {
+
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        // PartitionStepBuilder 구현체 생성
+        return stepBuilderFactory.get("step3")
+                .partitioner(step1())
+                .gridSize(2)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        // JobStepBuilder 구현체 생성
+        return stepBuilderFactory.get("step4")
+                .job(job())
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        // FlowStepBuilder 구현체 생성
+        return stepBuilderFactory.get("step5")
+                .flow(flow())
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean
+    public Flow flow(){
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step2()).end();
+        return flowBuilder.build();
+    }
+
+}
