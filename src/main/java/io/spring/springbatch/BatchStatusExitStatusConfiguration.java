6691ce87678858b600eb71a6908fd6de2bcfcd97
lsm0506 <tkdals2317@gmail.com>
2023-08-17 22:08:59 +0900

    Part4.3.1.3 섹션 6 Transition - 배치 상태 유형(BatchStatus/ ExitStatus/ FlowExecutionStatus)

---
+++ src/main/java/io/spring/springbatch/BatchStatusExitStatusConfiguration.java
@@ -0,0 +1,74 @@
+package io.spring.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class BatchStatusExitStatusConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+/*
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+*/
+
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .on("FAILED")
+                .to(step2())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> step1 has executed");
+                        contribution.setExitStatus(ExitStatus.FAILED);
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> step2 has executed");
+                        // BATCH_JOB_EXECUTION에서 STATUS는 COMPLETED, EXIT_CODE가 FAILED로 반영된다.
+                        // 마지막 Step의 ExitStatus가 Job의 ExitCode로 반영되기 때문이다.
+                        //contribution.setExitStatus(ExitStatus.FAILED);
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+}
