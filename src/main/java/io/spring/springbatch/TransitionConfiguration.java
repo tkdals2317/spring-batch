128584e633a58113afc87f896348e0b0d4825f77
lsm0506 <tkdals2317@gmail.com>
2023-08-17 22:52:42 +0900

    Part4.3.1.4 섹션 6 Transition - on() / to() / stop() / fail() / stopAndRestart()

--- src/main/java/io/spring/springbatch/TransitionConfiguration.java
+++ src/main/java/io/spring/springbatch/TransitionConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -17,30 +15,30 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class BatchStatusExitStatusConfiguration {
+public class TransitionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-/*
     @Bean
     public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
                 .start(step1())
-                .next(step2())
-                .build();
-    }
-*/
-
-    @Bean
-    public Job batchJob() {
-        return this.jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .on("FAILED")
-                .to(step2())
+                    .on("FAILED")
+                    .to(step2())
+                    .on("FAILED")
+                    .stop()
+                .from(step1())
+                    .on("*")
+                    .to(step3())
+                    .next(step4())
+                .from(step2())
+                    .on("*")
+                    .to(step5())
                 .end()
                 .build();
     }
+
 
     @Bean
     public Step step1() {
@@ -62,13 +60,48 @@ public class BatchStatusExitStatusConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println(">> step2 has executed");
-                        // BATCH_JOB_EXECUTION에서 STATUS는 COMPLETED, EXIT_CODE가 FAILED로 반영된다.
-                        // 마지막 Step의 ExitStatus가 Job의 ExitCode로 반영되기 때문이다.
-                        //contribution.setExitStatus(ExitStatus.FAILED);
+                        contribution.setExitStatus(ExitStatus.FAILED);
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> step3 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> step4 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> step5 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
 
 }
