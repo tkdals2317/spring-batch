e162ccdf31e79030d9c864be4a828744964e7e77
lsm0506 <tkdals2317@gmail.com>
2023-08-18 01:56:39 +0900

    Part4.3.1.3.4 섹션 6 JobExecutionDecider

--- src/main/java/io/spring/springbatch/JobExecutionDeciderConfiguration.java
+++ src/main/java/io/spring/springbatch/JobExecutionDeciderConfiguration.java
@@ -1,12 +1,12 @@
 package io.spring.springbatch;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class CustomExitStatusConfiguration {
+public class JobExecutionDeciderConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -23,43 +23,56 @@ public class CustomExitStatusConfiguration {
     @Bean
     public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
-                .start(step1())
-                    .on("FAILED")
-                    .to(step2())
-                    .on("PASS") // PASS가 아닐 경우 어떻게 하라고 정의되있지 않아 실패로 간주하여 JOB_EXECUTION 테이블에 FAILED로 저장
-                    .stop()
+                .start(step())
+                .next(decider())
+                .from(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(evenStep())
                 .end()
                 .build();
     }
 
+    @Bean
+    public JobExecutionDecider decider() {
+        return new CustomDecider();
+    }
 
     @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
+    public Step step() {
+        return stepBuilderFactory.get("step")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step1 has executed");
-                        //contribution.setExitStatus(ExitStatus.FAILED);
-                        contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
+                        System.out.println(">> This is the start tasklet");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
+
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step2 has executed");
+                        System.out.println(">> evenStep has executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
-                .listener(new PassingCheckingListener())
                 .build();
     }
 
+    @Bean
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> oddStep has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
 
 }
