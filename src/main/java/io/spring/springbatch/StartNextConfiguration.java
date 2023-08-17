41f5e63d8842dccd76750597c37a9acb153d7767
lsm0506 <lsm0506@midasin.com>
2023-08-17 14:04:53 +0900

    Part4.2.2.2 SimpleJob - start()/next()

--- src/main/java/io/spring/springbatch/StartNextConfiguration.java
+++ src/main/java/io/spring/springbatch/StartNextConfiguration.java
@@ -1,18 +1,10 @@
 package io.spring.springbatch;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class SimpleJobConfiguration {
+public class StartNextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -31,25 +23,6 @@ public class SimpleJobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
-                .incrementer(new RunIdIncrementer())
-                .validator(new JobParametersValidator() {
-                    @Override
-                    public void validate(JobParameters jobParameters) throws JobParametersInvalidException {
-
-                    }
-                })
-                .preventRestart()
-                .listener(new JobExecutionListener() {
-                    @Override
-                    public void beforeJob(JobExecution jobExecution) {
-
-                    }
-
-                    @Override
-                    public void afterJob(JobExecution jobExecution) {
-
-                    }
-                })
                 .build();
     }
 
@@ -78,9 +51,6 @@ public class SimpleJobConfiguration {
     public Step step3() {
         return stepBuilderFactory.get("step3")
                 .tasklet((contribution, chunkContext) -> {
-                    // 최종 스텝의 상태가 JobExecution에 반영된다.
-                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-                    contribution.setExitStatus(ExitStatus.STOPPED);
                     System.out.println(">> step3 has executed!");
                     return RepeatStatus.FINISHED;
                 })
