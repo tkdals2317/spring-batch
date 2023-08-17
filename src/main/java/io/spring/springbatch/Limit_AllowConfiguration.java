d1666b50a4f72ddc61894e1a2b3d1c1ef5798445
lsm0506 <lsm0506@midasin.com>
2023-08-17 16:53:11 +0900

    Part4.2.4.3 TaskletStep - startLimit() / allowStartIfComplete()

--- src/main/java/io/spring/springbatch/Limit_AllowConfiguration.java
+++ src/main/java/io/spring/springbatch/Limit_AllowConfiguration.java
@@ -15,20 +15,19 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class TaskletConfiguration {
+public class Limit_AllowConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final CustomTasklet customTasklet;
 
     @Bean
     public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
-                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build();
     }
+
 
     @Bean
     public Step step1() {
@@ -40,12 +39,22 @@ public class TaskletConfiguration {
                         return RepeatStatus.FINISHED;
                     }
                 })
+                .allowStartIfComplete(true) // 성공 했던 step이라도 다시 실행한다.
                 .build();
     }
+
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(customTasklet)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
+                        throw new RuntimeException("step2 was failed");
+//                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .startLimit(3) // 4번째 Step 실행 시 StartLimitExceededException 발생!
                 .build();
     }
 }
