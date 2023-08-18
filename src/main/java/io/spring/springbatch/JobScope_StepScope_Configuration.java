2a4721a6862fbd30a7e3271dd2ce385c5c0e170b
lsm0506 <tkdals2317@gmail.com>
2023-08-18 20:57:26 +0900

    Part4.4.2 섹션 6 @JobScope / @StepScope - 기본개념 및 설정

---
+++ src/main/java/io/spring/springbatch/JobScope_StepScope_Configuration.java
@@ -0,0 +1,61 @@
+package io.spring.springbatch;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobScope_StepScope_Configuration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1(null))
+                .next(step2())
+                .listener(new JobListener())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step1(@Value("#{jobParameters['message']}") String message) {
+
+        System.out.println("jobParameters['message'] : " + message);
+        return stepBuilderFactory.get("step1")
+                .tasklet(tasklet1(null))
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet tasklet1(@Value("#{jobExecutionContext['name']}") String name){
+        return (stepContribution, chunkContext) -> {
+            System.out.println("jobExecutionContext['name'] : " + name);
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
