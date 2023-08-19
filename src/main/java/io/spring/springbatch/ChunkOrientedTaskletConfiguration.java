5760f59a21c9e399a2c343af61db3c48033a5148
lsm0506 <tkdals2317@gmail.com>
2023-08-19 22:37:18 +0900

    Part5.1 ChunkOrientedTasklet - 개념 및 API 소개

--- src/main/java/io/spring/springbatch/ChunkOrientedTaskletConfiguration.java
+++ src/main/java/io/spring/springbatch/ChunkOrientedTaskletConfiguration.java
@@ -6,13 +6,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,7 +18,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class ChunkConfiguration {
+public class ChunkOrientedTaskletConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -37,20 +34,17 @@ public class ChunkConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(5)
+                .<String, String>chunk(2)
                 .reader(new ListItemReader<>(Arrays.asList("item1","item2","item3","item4","item5","item6")))
                 .processor(new ItemProcessor<String, String>() {
                     @Override
                     public String process(String item) throws Exception {
-                        Thread.sleep(300);
-                        System.out.println("item = " + item);
-                        return "my" + item;
+                        return "my_" + item;
                     }
                 })
                 .writer(new ItemWriter<String>() {
                     @Override
                     public void write(List<? extends String> list) throws Exception {
-                        Thread.sleep(300);
                         System.out.println(list);
                     }
                 })
