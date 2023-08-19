5160e06f13bd44135c32e2fbd83f2eeec53e60b4
lsm0506 <tkdals2317@gmail.com>
2023-08-20 02:58:34 +0900

    Part6.1.4.2 DB - JpaCursorItemReader

--- src/main/java/io/spring/springbatch/Customer.java
+++ src/main/java/io/spring/springbatch/Customer.java
@@ -1,15 +1,23 @@
 package io.spring.springbatch;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
+@Entity
 public class Customer {
 
+    @Id
+    @GeneratedValue
     private long id;
-    private String firstName;
-    private String lastName;
+
+    private String firstname;
+
+    private String lastname;
+
     private String birthdate;
 
 }
--- src/main/java/io/spring/springbatch/JpaCursorConfiguration.java
+++ src/main/java/io/spring/springbatch/JpaCursorConfiguration.java
@@ -9,22 +9,23 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
-import org.springframework.batch.item.json.JacksonJsonObjectReader;
-import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @Configuration
-public class JdbcCursorConfiguration {
+public class JpaCursorConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private int chunkSize = 10;
+    private final EntityManagerFactory entityManagerFactory;
+    private int chunkSize = 5;
     private final DataSource dataSource;
 
     @Bean
@@ -39,20 +40,21 @@ public class JdbcCursorConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(chunkSize)
-                .reader(customerItemReader())
+                .reader(customItemReader())
                 .writer(customItemWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader<Customer> customerItemReader() {
-        return new JdbcCursorItemReaderBuilder<Customer>()
-                .name("jdbcCursorItemReader")
-                .fetchSize(chunkSize)
-                .sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
-                .beanRowMapper(Customer.class)
-                .queryArguments("A%")
-                .dataSource(dataSource)
+    public ItemReader<Customer> customItemReader() {
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("firstname", "A%");
+
+        return new JpaCursorItemReaderBuilder<Customer>()
+                .name("jpaCursorItemReaderBuilder")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select c from Customer c where firstName like :firstname")
+                .parameterValues(parameters)
                 .build();
     }
 
